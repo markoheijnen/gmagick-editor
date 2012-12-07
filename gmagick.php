02e00e042e2123ef64fc50207703f68acd8bc4a7
Mike Schroder <mike.schroder@dreamhost.com>
2012-12-06 20:04:59 -0800

    Initial headers for GMagick plugin

--- gmagick.php
+++ gmagick.php
@@ -1,4 +1,13 @@
 <?php
+/*
+Plugin Name: WP_Image_Editor_Gmagick
+Plugin URI: http://wordpress.org/extend/plugins/wp-image-editor-gmagick
+Description: Enables Gmagick in WordPress
+Author: Marko Heijnen and Mike Schroder
+Version: 0.1
+Author URI: https://github.com/markoheijnen/gmagick-editor/
+*/
+
 function image_editors_add_gmagick( $editors ) {
 	include 'editors/gmagick.php';
 	array_unshift( $editors, 'WP_Image_Editor_Gmagick' );
